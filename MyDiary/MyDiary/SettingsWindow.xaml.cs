d161f08bafb5abfd17c77be02e3d62d36466bc40
JNPRZ2000 <3143611380710@ingenieria.usac.edu.gt>
2024-07-30 13:26:28 -0600

    color picker and design

--- MyDiary/MyDiary/SettingsWindow.xaml.cs
+++ MyDiary/MyDiary/SettingsWindow.xaml.cs
@@ -11,8 +11,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,6 +43,33 @@ namespace MyDiary
         {
 
             AppInstance.Restart("Restart");
+            /*var color = colorPicker.Color.ToString();
+            await ShowErrorDialogAsync(color);*/
+        }
+        private async Task ShowErrorDialogAsync(string message)
+        {
+            var hwnd = WindowNative.GetWindowHandle(this);
+            var dialog = new MessageDialog(message, "Error");
+            InitializeWithWindow.Initialize(dialog, hwnd);
+            await dialog.ShowAsync();
+        }
+
+        private void CbbSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void CbbSize_TextSubmitted(ComboBox sender, ComboBoxTextSubmittedEventArgs args)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox == null) { return; }
+            var text = combobox.Text;
+            if (!Regex.IsMatch(text, @"^[1-9]\d*$"))
+            {
+                lblSize.Content = "Font Size: not num- " + text;
+                //ShowErrorDialogAsync("Is not num: " + text);
+            }
+            else lblSize.Content = "Font Size:";
         }
     }
 }
